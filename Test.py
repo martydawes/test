cbbb56b8703df3932f2341f9922e393a42864f9a
martydawes <66390712+martydawes@users.noreply.github.com>
2022-11-19 15:19:44 +0000

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,15 @@
+first_name= input ('enter first name: ')
+last_name= input('enter last name: ')
+print('hello', first_name,  last_name)
+
+
+age=int(input('please tell me your age:'))
+
+if age <= 45: 
+    print('you are so young')
+else: 
+    print('you are so old')
+    
+
+
+
